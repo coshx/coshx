8c928a694ee2e285f1fcc54d39c33368ce982667
Mike Hickman <mhickman84@gmail.com>
2011-09-05 22:18:38 -0400

    nav styles and content

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,11 @@
 class HomeController < ApplicationController
   def index; end
+
+  def services; end
+
+  def work; end
+
+  def blog; end
+
+  def about; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,10 @@ Coshx::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'home#index'
-
+  match '/services' => 'home#services', :as => :services
+  match '/our_work' => 'home#work', :as => :work
+  match '/our_blog' => 'home#blog', :as => :blog
+  match '/about' => 'home#about', :as => :about
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
