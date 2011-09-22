f6da5a114eb20ce83db6fa81b6d28212029c36d1
Mike Hickman <mhickman84@gmail.com>
2011-09-21 23:25:37 -0400

    adds ability to save blog posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-
+    @posts = Post.all
   end
 
   def new
@@ -8,6 +8,19 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
+    end
+  end
+
+  def create
+    post = Post.new(:author => current_admin, :title => params[:post][:title], :body => params[:post][:body])
+    post.date = DateTime.now
+
+    respond_to do |format|
+      if post.save
+        format.html { redirect_to admin_root_path, notice: 'Blog post saved' }
+      else
+        format.html { redirect_to admin_root_path, error: 'Error saving blog post.' }
+      end
     end
   end
 end
