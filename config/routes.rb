72f9461d6d6313a5ceef3ffcc32514004b33e734
Mike Hickman <mhickman84@gmail.com>
2011-09-26 18:38:38 -0400

    removes unnecessary comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,65 +1,12 @@
 Coshx::Application.routes.draw do
-
   devise_for :admins
+  
   match 'dashboard' => 'dashboard#index', :as => :admin_root
+
   resources :posts, :path => "/blog"
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => 'home#index'
+  get "/posts/*id" => 'posts#show', :as => :post, :constraints => {:id => %r[\d{4}/\d{2}/\d{2}/[^/]+]}
   match '/services' => 'home#services', :as => :services
   match '/portfolio' => 'home#portfolio', :as => :portfolio
   match '/about' => 'home#about', :as => :about
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  root :to => 'home#index'
 end
