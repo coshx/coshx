07e39be711892b2acd2877b6107f4e1cd9c59dbb
David Kapp <davidkapp@coshx.com>
2011-12-02 14:38:12 -0700

    regenerate schema to fix merge

--- db/schema.rb
+++ db/schema.rb
@@ -1,1 +1,58 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
 
+ActiveRecord::Schema.define(:version => 20111201233358) do
+
+  create_table "admins", :force => true do |t|
+    t.string   "email",                                 :default => "",                                         :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "",                                         :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                         :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name"
+    t.text     "bio",                                   :default => "I''m a developer at Coshx and I love it!"
+    t.string   "twitter"
+    t.string   "github"
+  end
+
+  add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
+  add_index "admins", ["reset_password_token"], :name => "index_admins_on_reset_password_token", :unique => true
+
+  create_table "clients", :force => true do |t|
+    t.string   "name"
+    t.string   "site_url"
+    t.text     "quote"
+    t.string   "quoted_person"
+    t.text     "what_we_did"
+    t.string   "company_logo"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "author_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.datetime "posted_on"
+  end
+
+end
