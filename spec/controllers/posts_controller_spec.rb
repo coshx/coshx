1614d3b2862af9b4d076eb5e72bce282c725bc0c
Mike Hickman <mhickman84@gmail.com>
2011-12-06 17:10:35 -0500

    fixes controller spec

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe PostsController do
   describe "feed" do
     it "renders posts in atom format" do
-      get :feed
+      get :feed, :format => :atom
       assigns(:posts)
     end
   end
