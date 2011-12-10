57935edb7f11c2862d448f6fbd5d77581c0a6805
Mike Hickman <mhickman84@gmail.com>
2011-12-10 16:45:03 -0500

    creates class macro for handling markdonw attributes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ActiveRecord::Base
+  extend MarkdownAttributes
+
   belongs_to :author, :class_name => "Admin"
   default_scope :order => "created_at DESC"
+
+  attr_markdown :preview, :body
 
   def preview
     lines = self.body.split(/(\n)+/)
@@ -8,22 +12,6 @@ class Post < ActiveRecord::Base
       line =~ /^$\n/
     end.take(4).join(' ')
     "#{preview_text.chomp}..."
-  end
-
-  def body_html
-    if self.body
-      Post.format_as_html(self.body)
-    else
-      nil
-    end
-  end
-
-  def preview_html
-    if self.preview
-      Post.format_as_html(self.preview)
-    else
-      nil
-    end
   end
 
   def publish
@@ -35,22 +23,5 @@ class Post < ActiveRecord::Base
 
   def published?
     true unless self.posted_on.nil?
-  end
-
-  private
-  def self.highlight_syntax(html)
-    doc = Nokogiri::HTML::fragment(html)
-    doc.search("code").each do |code_tag|
-      unless code_tag[:class].nil?
-        code_tag.replace Pygmentize.process(code_tag.text.rstrip, code_tag[:class].intern)
-      end
-    end
-    doc.to_s
-  end
-
-  def self.format_as_html(text)
-    markdown_renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :fenced_code_blocks => true)
-    html = markdown_renderer.render(text)
-    Post.highlight_syntax(html)
   end
 end
