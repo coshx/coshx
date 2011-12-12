90660456ee32f4682f163fc07aac3b541149e182
Mike Hickman <mhickman84@gmail.com>
2011-12-11 21:41:45 -0500

    changes job description field to text to allow for long descriptions

---
+++ db/migrate/20111212015115_change_job_description_column_type.rb
@@ -0,0 +1,9 @@
+class ChangeJobDescriptionColumnType < ActiveRecord::Migration
+  def up
+    change_column :jobs, :description, :text
+  end
+
+  def down
+    change_column :jobs, :description, :string
+  end
+end
