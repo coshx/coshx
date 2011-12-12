be5e8d19866f21b5e3b8994b4c34cd98bacbdc7a
Mike Hickman <mhickman84@gmail.com>
2011-12-11 20:02:37 -0500

    adds ability to view and edit job postings

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,13 @@
 class JobsController < ApplicationController
+  def index
+    if admin_signed_in?
+      @jobs = Job.all
+    else
+      @jobs = Job.select { |p| p.published? }
+    end
+  end
+
+
   def new
     @job = Job.new
 
@@ -8,13 +17,43 @@ class JobsController < ApplicationController
   end
 
   def create
-    job = Job.new(:author => current_admin, :title => params[:job][:title], :description => params[:job][:description])
+    job = Job.new(:title => params[:job][:title], :description => params[:job][:description])
 
     respond_to do |format|
       if job.save
-        format.html { redirect_to admin_root_path, notice: 'Job posting saved' }
+        format.html { redirect_to jobs_path, notice: 'Job posting saved' }
       else
-        format.html { redirect_to admin_root_path, error: 'Error saving job posting.' }
+        format.html { redirect_to @job, error: 'Error saving job posting.' }
+      end
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    respond_to do |format|
+      if @job.update_attributes(params[:job])
+        format.html { redirect_to @job, :notice => 'Post was successfully updated.' }
+      else
+        format.html { render :action => "edit" }
+      end
+    end
+  end
+
+  def show
+    @job ||= Job.find(params[:id])
+  end
+
+  def publish
+    @job = Job.find(params[:id])
+    respond_to do |format|
+      if @job.publish && @job.save
+        format.html { redirect_to :action => "index", :notice => 'Job has been published' }
+      else
+        format.html { redirect_to :action => "index" }
       end
     end
   end
