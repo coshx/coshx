fd5611dc811e6f52a08ff12d751bc3f995cdcbbe
Ryan Ahearn <ryan@coshx.com>
2012-01-10 15:58:30 -0500

    Adds email initializer for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,7 @@ Coshx::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => ENV['COSHX_HOST'] }
 
   # Enable threaded mode
   # config.threadsafe!
