7812d01441778071c96a623ca41ab65d86842160
Ryan Ahearn <ryan@coshx.com>
2012-01-13 15:12:48 -0500

    Tightens up Publishable module, adds test for published scope

--- spec/lib/publishable_spec.rb
+++ spec/lib/publishable_spec.rb
@@ -1,31 +1,39 @@
 require 'spec_helper'
 
 describe Publishable do
-  before :each do
-    class Tester; attr_accessor :posted_on; end
-    @object = Tester.new
-    @object.extend Publishable
+  before(:all) do
+    class Tester
+      include Publishable
+      attr_accessor :posted_on
+    end
   end
+  subject { Tester.new }
 
-  it "sets posted_on to the current date/time" do
-    Timecop.freeze(DateTime.now) do
-      @object.publish
-      @object.posted_on.should == Time.now
+  describe "#publish" do
+    it "sets posted_on to the current date/time" do
+      Timecop.freeze(DateTime.now) do
+        subject.publish.posted_on.should == Time.now
+      end
+    end
+
+    it "sets published? to true" do
+      subject.publish.published?.should be_true
+    end
+
+
+    it "does nothing on subsequent calls" do
+      date = DateTime.parse("2008-01-01")
+      Timecop.freeze(date) do
+        subject.publish
+      end
+      subject.publish.posted_on.should == date
     end
   end
 
-  it "sets published? to true" do
-    @object.publish
-    @object.published?.should be_true
-  end
-
-
-  it "does nothing on subsequent calls" do
-    date = DateTime.parse("2008-01-01")
-    Timecop.freeze(date) do
-      @object.publish
+  describe ".published" do
+    it "limits results to rows where posted_on is set" do
+      Tester.should_receive(:where).with 'posted_on IS NOT NULL'
+      Tester.published
     end
-    @object.publish
-    @object.posted_on.should == date
   end
 end
