b61dffcce386d75894a5890be78ffe192885a4fb
Calvin Delamere <calvin@coshx.com>
2012-04-05 13:45:24 -0600

    error handling when twitter post fails

--- lib/tweeter.rb
+++ lib/tweeter.rb
@@ -1,7 +1,11 @@
 module Tweeter
   def self.blog_post_tweet(post)
     tweet = trim_tweet("Check out #{post.author.name}'s blog post on coshx.com/blog about \"#{post.title}\"")
-    Twitter.update(tweet)
+    begin
+      Twitter.update(tweet)
+    rescue => err
+      Rails.logger.error "Unable to tweet blog post. Post and err below\n\n #{post.inspect} \n\n #{err.inspect}"
+    end
   end
 
   def self.trim_tweet(tweet)
