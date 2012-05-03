8519db8e66d4c0599f6bffd3b621e10c0f53491d
Calvin Delamere <calvin@coshx.com>
2012-05-03 13:08:27 -0600

    get actual blog posts in the footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :last_two_posts
+  def last_two_posts
+    @last_two_posts ||= Post.published.limit(2)
+  end
 end
