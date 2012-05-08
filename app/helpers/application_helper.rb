de8ff9bdc60aa92b52f11b6466d3e1b98fa27d37
Calvin Delamere <calvin@coshx.com>
2012-05-08 16:07:41 -0600

    update nav to match gils css, applied selected css to current tab with tab_to

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,20 @@ module ApplicationHelper
     (@body_class || :default).to_s
   end
 
+  #This does not support blocks like link_to does
+  def tab_to(*args)
+    name         = args[0]
+    url_options  = args[1] || {}
+    html_options = args[2]
+    url = url_for(url_options)
+
+    link = link_to(name, url, html_options)
+
+    if request.path == url
+      raw "<li class ='selected'>" + link + "</li>"
+    else
+      raw "<li>" + link + "</li>"
+    end
+  end
+
 end
