750b6d624afa31514e85ce8662c37aa12f020b46
Sang Won Seo <sang@coshx.com>
2012-06-04 15:51:40 -0400

    original state

--- config/application.rb
+++ config/application.rb
@@ -51,7 +51,6 @@ module Coshx
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
-    config.assets.paths << Rails.root.join("app", "assets", "images", "team")
 
     config.generators do |g|
       g.helper false
