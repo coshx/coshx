a47155217bf65a93c7b09c4841152de065f2bf23
Michael <info1117@gmail.com>
2012-08-09 16:33:29 -0700

    helper with current controller and action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  helper_method :last_two_posts, :random_quote
+  helper_method :last_two_posts, :random_quote, :whereami
+  
   
   def last_two_posts
     @last_two_posts ||= Post.published.limit(2)
@@ -10,4 +11,10 @@ class ApplicationController < ActionController::Base
   def random_quote
     Quote.first(:order => "RANDOM()")
   end
+  
+  def whereami
+     params[:controller].to_s + "/" + params[:action]
+     #     controller.controller_name.to_s + "/" + controller.action_name
+  end
+  
 end
