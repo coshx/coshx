c4d94bf8d4ac243f6bcf9b9d2b1c50dc29116bde
Michael <info1117@gmail.com>
2012-08-17 14:02:32 -0700

    tweaking blogs and css for it

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -5,6 +5,7 @@ class ContentsController < ApplicationController
   # GET /contents
   # GET /contents.json
   def index
+
     @contents = Content.all
 
     respond_to do |format|
--- app/models/content.rb
+++ app/models/content.rb
@@ -1,3 +1,5 @@
 class Content < ActiveRecord::Base
   attr_accessible :action, :index, :value
+  
+    validates_presence_of :action
 end
