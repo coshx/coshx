ee815a80e2cfd41e21d9dd71a8285fef5352dc0a
Ben Taitelbaum <ben@coshx.com>
2012-08-22 20:49:14 -0400

    Sets blog author link
     - because generate_slug is called on before save,
       ensure that each user gets saved with the next seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@ ben = Admin.find_or_create_by_email("ben@coshx.com") do |b|
   b.bio = 'Almost two decades ago, twelve-year-old Ben reproduced the classic computer game <i>Nibbles</i>, providing hours of riveting entertainment. Since then, he has never strayed from manipulating technology for the benefit of its users. Having spent the last three years as the founder of a start-up company and then forming Coshx Labs, Ben is always on the lookout for meaningful, game-changing ideas! When he is not busy attempting to change the world, he may be found scaling the ascents of the Blue Ridge Mountains or serenading to the tunes of musicians from Josh Ritter to Johannes Brahms on his trusty violin.'
   b.img = "ben.png"
 end
+ben.save!
 puts "Created User #{ben.name}"
 
 mike = Admin.find_or_create_by_email("mike@coshx.com") do |m|
@@ -18,6 +19,7 @@ mike = Admin.find_or_create_by_email("mike@coshx.com") do |m|
   m.bio = "Prior to joining Coshx Labs, Mike developed web applications in subject areas as diverse as event registration and biofuel trading. As a front and back-end developer, he enjoys distilling complex problems into elegant and obvious interfaces that even your mother-in-law could use. Outside of work, Mike enjoys fly-fishing, mountain biking and playing fetch with Walker, his labrador retriever."
   m.img = "mike.png"
 end
+mike.save!
 puts "Created User #{mike.name}"
 
 david = Admin.find_or_create_by_email("david@coshx.com") do |d|
@@ -28,6 +30,7 @@ david = Admin.find_or_create_by_email("david@coshx.com") do |d|
   d.bio = "David loves watching online startups rapidly transform life as we know it for the better. An old C++ programmer, he shifted to Ruby several years ago so he could join the party. He loves the flow of writing clean code using TDD, pairing, and advocating agility internally and for established clients. Outside of work David enjoys climbing, cycling, kayaking, and spending time with his wife."
   d.img = "david-kovsky.png"
 end
+david.save!
 puts "Created User #{david.name}"
 
 dave_kapp = Admin.find_or_create_by_email("davidkapp@coshx.com") do |d|
@@ -38,6 +41,7 @@ dave_kapp = Admin.find_or_create_by_email("davidkapp@coshx.com") do |d|
   d.bio = "In a former life, Dave had a degree in Asian Studies and taught English in Japan. He was later reborn as a software developer and did C++ and Java for a few years before finding the awesomeness of Ruby and other dynamic languages. He lives in Littleton, CO with his fiance, Madeline, and two wonderful kitties. His spare time is eaten up by studying Japanese, videogames, manga, anime, and learning about new programming languages and techniques."
   d.img = "david-kapp.png"
 end
+dave_kapp.save!
 puts "Created User #{dave_kapp.name}"
 
 ryan = Admin.find_or_create_by_email("ryan@coshx.com") do |r|
@@ -48,6 +52,7 @@ ryan = Admin.find_or_create_by_email("ryan@coshx.com") do |r|
   r.bio = "Ryan got started programming with his first QBASIC class in high school and hasn't looked back since. He's tried his hand at most programming languages and is most passionate about finding the simplest solution to any given problem. When not working, Ryan participates in most every recreational sport he can find in Baltimore and cheers on the New York Giants and Yankees."
   r.img = "ryan.png"
 end
+ryan.save!
 puts "Created User #{ryan.name}"
 
 gabe = Admin.find_or_create_by_email("gabe@coshx.com") do |g|
@@ -58,6 +63,7 @@ gabe = Admin.find_or_create_by_email("gabe@coshx.com") do |g|
   g.github = "gkop"
   g.img = "gabe.png"
 end
+gabe.save!
 puts "Created User #{gabe.name}"
 
 chielo = Admin.find_or_create_by_email("chielo@coshx.com") do |c|
@@ -66,6 +72,7 @@ chielo = Admin.find_or_create_by_email("chielo@coshx.com") do |c|
   c.bio = "Laying Ruby code like concrete blocks, Chielo is a builder by trade and obsession.  He enjoys working and finds it highly rewarding to deliver value and quality through beautiful programming."
   c.img = "chielo.png"
 end
+chielo.save!
 puts "Created User #{chielo.name}"
 
 ant = Admin.find_or_create_by_email("ant@coshx.com") do |a|
@@ -76,6 +83,7 @@ ant = Admin.find_or_create_by_email("ant@coshx.com") do |a|
   a.twitter = "anthonyburton"
   a.img = "anthony.png"
 end
+ant.save!
 puts "Created User #{ant.name}"
 
 calvin = Admin.find_or_create_by_email("calvin@coshx.com") do |c|
@@ -84,6 +92,7 @@ calvin = Admin.find_or_create_by_email("calvin@coshx.com") do |c|
   c.bio = "Although Calvin is the youngest software engineer at Coshx, he started programming nine years ago and built his first website eight years ago. In the last few years he has focused his computer science and applied math background on web development. In his free time, Calvin enjoys skiing, biking, bouldering, and ultimate frisbee. Calvin also enjoys crushing the competition at foosball."
   c.img = "calvin.png"
 end
+calvin.save!
 puts "Created User #{calvin.name}"
 
 sang = Admin.find_or_create_by_email("sang@coshx.com") do |s|
@@ -94,6 +103,7 @@ sang = Admin.find_or_create_by_email("sang@coshx.com") do |s|
     s.bio = "Sang is an intern at Coshx. He is currently studying computer science at University of Virginia. Although he is not as experienced as other developers at Coshx, he is eager to learn more and grow as a developer. Other than programming, Sang enjoys weight training, listening to music, and playing instruments."
     s.img = "sang.png"
 end
+sang.save!
 puts "Created user #{sang.name}"
 
 mm = Admin.find_or_create_by_email("mikhail@coshx.com") do |s|
@@ -104,6 +114,7 @@ mm = Admin.find_or_create_by_email("mikhail@coshx.com") do |s|
     s.bio = "Michael loves code, he worked in Japan with databases, he a huge front-end experience, and on his free time he teaches a Rails class in San Francisco"
     s.img = "m.png"
 end
+mm.save!
 puts "Created user #{mm.name}"
 
 
@@ -116,6 +127,8 @@ gil = Admin.find_or_create_by_email("gil@coshx.com") do |g|
   g.github = "edigil"
   g.img = "gil.png"
 end
+gil.save!
+puts "Created user #{gil.name}"
 
 # -----------------------------------------------------------------------------
 # clients
