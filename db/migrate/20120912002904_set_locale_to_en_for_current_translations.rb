19a64408e22350af87cc5a63914224bc78302334
Gabe Kopley <gabe@coshx.com>
2012-09-11 17:46:06 -0700

    Bump gem, add initializers, set locale for existing rows

---
+++ db/migrate/20120912002904_set_locale_to_en_for_current_translations.rb
@@ -0,0 +1,12 @@
+class SetLocaleToEnForCurrentTranslations < ActiveRecord::Migration
+  def up
+    Contenteditable::Translation.all.each do |t|
+      t.locale = "en"
+      t.save!
+    end
+  end
+
+  def down
+    # noop
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120912001059) do
+ActiveRecord::Schema.define(:version => 20120912002904) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                                 :default => "",                                         :null => false
