97be8405f0564a15c660839b3bee2f9d94e8a298
Walt House <walt@coshx.com>
2012-10-08 16:13:10 -0600

    AZ-53 finishes specs for error pages

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,27 +2,20 @@ require 'spec_helper'
 
 describe ApplicationController do
   describe "#catch_exceptions" do
-    context "not in production" do
-      it "rescues a 505" do
-        @controller.send(:catch_exceptions) {raise "HELL"}
-        expect { @controller.send(:catch_exceptions) }.should raise_error
-      end
-    end
-
     context "in production" do
       before(:each) do
         Rails.stub_chain(:env, :production?).and_return(true)
       end
 
       it "rescues a 505" do
+        @controller.should_receive(:render_error)
         @controller.send(:catch_exceptions) {raise "HELL"}
-        expect { @controller.send(:catch_exceptions) }.should raise_error
       end
 
       it "renders page not found" do
         @controller.should_receive(:render_page_not_found)
+        @controller.send(:catch_exceptions) {raise ActiveRecord::RecordNotFound}
       end
     end
   end
-  #subject.render_page_not_found.should == "errors/not_found"
 end
