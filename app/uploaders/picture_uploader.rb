49916bd7daaac2919f5f2061188e0c24e3fda957
Walt House <moabwh@gmail.com>
2012-12-17 15:37:33 -0700

    Configures uploader for local testing

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,18 +2,6 @@
 
 class PictureUploader < CarrierWave::Uploader::Base
 
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
-  # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
-  # include Sprockets::Helpers::RailsHelper
-  # include Sprockets::Helpers::IsolatedHelper
-
-  # Choose what kind of storage to use for this uploader:
-  # storage :file
-  storage :fog
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,17 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => ENV['S3_KEY'],
-    :aws_secret_access_key  => ENV['S3_SECRET']
-  }
-  config.fog_directory  = 'coshx-website'
-  config.fog_public     = true
-#  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['S3_KEY'],
+      :aws_secret_access_key  => ENV['S3_SECRET']
+    }
+    config.fog_directory  = 'coshx-website'
+    config.fog_public     = true
+  #  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
+  else
+    config.permissions = 0666
+    config.directory_permissions = 0777
+    config.storage = :file
+  end
 end
