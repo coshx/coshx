b48549146ed2a07880359e1b60290841fdd8e747
Edi Gil <gil@coshx.com>
2013-01-22 16:58:43 +0100

    css,jquer,blog,blog post,fixes, tits

--- 2013/js/custom.js
+++ 2013/js/custom.js
@@ -1,7 +1,8 @@
 
 jQuery(function(){
-	$('#main-nav').localScroll({offset: {top:-140}, duration: 900 });
+	$('#main-nav').localScroll({offset: {top:-140}, duration: 900});
 	$('#logo, #logo-light').localScroll({duration: 900 });
+	$('section').localScroll({offset: {top:-140}, duration: 900});
 	$('.team-mate a, .project a').on({
 		mouseenter: function () {
 			$('.overlay', this).fadeIn();
