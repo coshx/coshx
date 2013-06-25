81a4d22f47efaab80f7744d3f23bf6d93933fa6e
Evan Derby <somekidwithhtml@gmail.com>
2013-06-25 14:16:59 -0600

    Cleaned up apostrophes

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121228004851) do
+ActiveRecord::Schema.define(:version => 20130625192436) do
 
   create_table "admins", :force => true do |t|
-    t.string   "email",                                 :default => "",                                           :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "",                                           :null => false
+    t.string   "email",                                 :default => "",                                               :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "",                                               :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(:version => 20121228004851) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
-    t.text     "bio",                                   :default => "I''''m a developer at Coshx and I love it!"
+    t.text     "bio",                                   :default => "I'm a developer at Coshx and I love it!"
     t.string   "twitter"
     t.string   "github"
     t.string   "img"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(:version => 20121228004851) do
     t.string   "dislikes"
     t.string   "slug"
     t.string   "nickname"
+    t.datetime "deleted_at"
   end
 
   add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
