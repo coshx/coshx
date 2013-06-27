7ae8fec7d8004a773d12bca38475aec855dcc331
Josh Rohrbaugh <jrohrbaugh@gmail.com>
2013-06-27 15:58:42 -0400

    twitter mock in test env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < ActionController::Base
 
   def latest_tweet
     @twitter_url = "coshxlabs"
-    if Rails.env.production?
+    if Rails.env.test?
       @user = OpenStruct.new ({:screen_name => "test_user"})
       @latest_tweet = OpenStruct.new ({:user => @user, :text => "This is a tweet", :id => 1, :created_at => "2013-06-26 13:10:23 -0400"} )
     else
