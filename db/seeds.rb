57bf0e6c95647004a31f68387b297b29dae5d89e
Josh Rohrbaugh <jrohrbaugh@gmail.com>
2013-07-29 17:24:33 -0400

    seeds update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,8 @@
-if ENV['RAILS_ENV'] == 'production'
-  raise "Seeds should not be run in production because they are out of date"
-end
 
 # -*- coding: utf-8 -*-
 require 'contenteditable'
 
 #ADMINS
-
-walt = Admin.find_or_create_by_email("walt@coshx.com") do |w|
-    w.name = 'Walt House'
-    w.password = 'password'
-    w.twitter = 'WaltMetro'
-    w.github = 'whouse04'
-    w.bio = 'New to programming, Walt is diving in without a lifevest. Walt is an avid outdoorsman(kayaking, ice climbing, canyoonering, and snow shoeing), foodie, musician, sci-fi junkie, and brewer. Walt graduated from The Ohio State University with a double-major in Political Science and National Security Policies. He served in U.S. Navy as an Intel Officer in San Diego. He did one tour in the gulf in 1999. After his discharge from the navy, he became a graphic designer.'
-    w.img = "walt.png"
-end
-walt.save!
-puts "Created User #{walt.name}"
 
 ben = Admin.find_or_create_by_email("ben@coshx.com") do |b|
   b.name = 'Ben Taitelbaum'
