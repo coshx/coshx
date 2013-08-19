9c042a516821e3c654a7bdc87b79ace764cdf954
Josh Rohrbaugh <jrohrbaugh@gmail.com>
2013-08-19 12:58:33 -0400

    updates to editing projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  attr_accessible :title, :product_description, :project_description, :result_description, :picture, :url, :featured_picture, :background_color
+  attr_accessible :title, :tagline, :product_description, :project_description, :result_description, :picture, :url, :featured_picture, :background_color
   mount_uploader :picture, PictureUploader
 	mount_uploader :featured_picture, PictureUploader
 
