d65dc4ea828689da4b646efa3cf01ba3b32b2cd9
David Kapp <davekapp@gmail.com>
2014-01-17 13:36:46 -0600

    fix migration that doesn't work for people who never had the Contenteditable stuff set up

--- db/migrate/20121008214451_strip_translation_values.rb
+++ db/migrate/20121008214451_strip_translation_values.rb
@@ -1,8 +1,13 @@
 class StripTranslationValues < ActiveRecord::Migration
   def up
-    Contenteditable::Translation.all.each do |t|
-      t.value = t.value.strip
-      t.save
+    #Contenteditable seems to have been removed so not sure if this migration is needed anymore, but leaving it for people who do have this - but with a rescue
+    begin
+      Contenteditable::Translation.all.each do |t|
+       t.value = t.value.strip
+       t.save
+      end
+    rescue => err
+      puts "Error in StripTranslationValues: #{err.to_s}"
     end
   end
 
