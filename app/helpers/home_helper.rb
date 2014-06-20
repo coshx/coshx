1649033c7a3a8c19b14ae09f9f4b8261a6729ab5
Evan Derby <somekidwithhtml@gmail.com>
2014-06-20 10:02:23 -0600

    Fixed wonky user positioning

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -17,17 +17,27 @@ module HomeHelper
 	
 private
 	def render_team_members(row)
+		enum = row.map #create an enumerator for the row array
 		content_tag('div', :id => 'team-row', :class => 'row') do
 			row.map do |person|
-				content_tag('div', render_team_member(person), :class => ['column', 'two', 'mobile-two'])
+				if enum.find_index(person)+1 == row.length and row.length > 1
+					content_tag('div', render_team_member(person), {:class => ['column', 'two', 'mobile-two'], :style=>"float:left"})
+				else
+					content_tag('div', render_team_member(person), :class => ['column', 'two', 'mobile-two'])
+				end
 			end.join.html_safe
 		end
 	end
 
 	def render_alumni_rows(row)
+		enum = row.map #create an enumerator for the row array
 		content_tag('div', :id => 'alumni-row', :class => 'row') do
 			row.map do |person|
-				content_tag('div', render_team_member(person), :class => ['column', 'two', 'mobile-two'])
+				if enum.find_index(person)+1 == row.length and row.length > 1
+					content_tag('div', render_team_member(person), {:class => ['column', 'two', 'mobile-two'], :style=>"float:left"})
+				else
+					content_tag('div', render_team_member(person), :class => ['column', 'two', 'mobile-two'])
+				end
 			end.join.html_safe
 		end
 	end
