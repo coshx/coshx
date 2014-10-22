4c3b0c8e610bba27191f73c44e160c8fbd9ca316
nbhartiya <neeharika@alum.mit.edu>
2014-10-22 16:29:09 -0400

    removes http from all src files

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -5,7 +5,7 @@ ready = function(e) {
 	window.stLight = undefined;
 
 	switchTo5x = true;
-	$.getScript("http://w.sharethis.com/button/buttons.js", function() {
+	$.getScript("//w.sharethis.com/button/buttons.js", function() {
 		stLight.options({publisher: "0a696786-6141-4757-805b-011d597ab233", doNotHash: false, doNotCopy: false, hashAddressBar: false});
 	});
 }
